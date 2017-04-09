58aa5f68e23f0b6770f68ca57475f893ddef8d9e
李進興 Vulcan <vulcan@miniasp.com>
2017-04-09 20:17:14 +0800

    Enable-Migrations

--- XamarinHandsOnLab_Runtime/XamarinHandsOnLabService/App_Start/Startup.MobileApp.cs
+++ XamarinHandsOnLab_Runtime/XamarinHandsOnLabService/App_Start/Startup.MobileApp.cs
@@ -9,6 +9,8 @@ using Microsoft.Azure.Mobile.Server.Config;
 using XamarinHandsOnLabService.DataObjects;
 using XamarinHandsOnLabService.Models;
 using Owin;
+using System.Data.Entity.Migrations;
+using XamarinHandsOnLabService.Migrations;
 
 namespace XamarinHandsOnLabService
 {
@@ -26,7 +28,10 @@ namespace XamarinHandsOnLabService
                 .ApplyTo(config);
 
             // Use Entity Framework Code First to create database tables based on your DbContext
-            Database.SetInitializer(new XamarinHandsOnLabInitializer());
+            //Database.SetInitializer(new XamarinHandsOnLabInitializer());
+
+            var migrator = new DbMigrator(new Migrations.Configuration());
+            migrator.Update();
 
             // To prevent Entity Framework from modifying your database schema, use a null database initializer
             // Database.SetInitializer<XamarinHandsOnLabContext>(null);
