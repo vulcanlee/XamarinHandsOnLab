94d52156fed044b2144b205b9c650a923fa2d9ac
李進興 Vulcan <vulcan@miniasp.com>
2017-04-15 01:30:03 +0800

    UploadImageController

---
+++ XamarinHandsOnLab_Runtime/XamarinHandsOnLabService/Controllers/UploadImageController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.Mobile.Server.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace XamarinHandsOnLabService.Controllers
+{
+    [MobileAppController]
+    public class UploadImageController : ApiController
+    {
+        [HttpPost]
+        public string Post()
+        {
+            var file = HttpContext.Current.Request.Files.Count > 0 ? HttpContext.Current.Request.Files[0] : null;
+
+            if (file != null && file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName) + Path.GetExtension(file.FileName);
+                fileName = Guid.NewGuid().ToString();
+
+                var path = Path.Combine(
+                    HttpContext.Current.Server.MapPath("~/Uploads"),
+                    fileName
+                );
+
+                file.SaveAs(path);
+            }
+
+            return file != null ? "/Uploads/" + file.FileName : "";
+        }
+    }
+}
