a739f9f2ed034c8a12e003d64f431a2ec93b64c6
李進興 Vulcan <vulcan@miniasp.com>
2017-04-15 14:30:44 +0800

    DoTasksController

---
+++ XamarinHandsOnLab_Runtime/XamarinHandsOnLabService/Controllers/DoTasksController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using XamarinHandsOnLabService.DataObjects;
+using XamarinHandsOnLabService.Models;
+using Microsoft.Azure.Mobile.Server.Config;
+
+namespace XamarinHandsOnLabService.Controllers
+{
+    [MobileAppController]
+    public class DoTasksController : Controller
+    {
+        private XamarinHandsOnLabContext db = new XamarinHandsOnLabContext();
+
+        // GET: DoTasks
+        public async Task<ActionResult> Index(string account)
+        {
+            var fooToday = DateTime.Now.Date;
+            return View(await db.UserTasks.Where(x=>DbFunctions.TruncateTime(x.TaskDateTime) == fooToday
+                                  && x.Account == account).ToListAsync());
+        }
+
+        // GET: DoTasks/Details/5
+        public async Task<ActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTasks userTasks = await db.UserTasks.FindAsync(id);
+            if (userTasks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTasks);
+        }
+
+        // GET: DoTasks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DoTasks/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Account,TaskDateTime,Status,Title,Description,CheckinId,Checkin_Latitude,Checkin_Longitude,CheckinDatetime,Condition1_Ttile,Condition1_Result,Condition2_Ttile,Condition2_Result,Condition3_Ttile,Condition3_Result,PhotoURL,Reported,ReportedDatetime")] UserTasks userTasks)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserTasks.Add(userTasks);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(userTasks);
+        }
+
+        // GET: DoTasks/Edit/5
+        public async Task<ActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTasks userTasks = await db.UserTasks.FindAsync(id);
+            if (userTasks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTasks);
+        }
+
+        // POST: DoTasks/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Account,TaskDateTime,Status,Title,Description,CheckinId,Checkin_Latitude,Checkin_Longitude,CheckinDatetime,Condition1_Ttile,Condition1_Result,Condition2_Ttile,Condition2_Result,Condition3_Ttile,Condition3_Result,PhotoURL,Reported,ReportedDatetime")] UserTasks userTasks)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userTasks).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(userTasks);
+        }
+
+        // GET: DoTasks/Delete/5
+        public async Task<ActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTasks userTasks = await db.UserTasks.FindAsync(id);
+            if (userTasks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTasks);
+        }
+
+        // POST: DoTasks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(long id)
+        {
+            UserTasks userTasks = await db.UserTasks.FindAsync(id);
+            db.UserTasks.Remove(userTasks);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ XamarinHandsOnLab_Runtime/XamarinHandsOnLabService/Helpers/QRHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZXing;
+using System.Drawing.Imaging;
+using ZXing.Common;
+
+namespace XamarinHandsOnLabService.Helpers
+{
+    public static class QRHelper
+    {
+        public static IHtmlString GenerateQrCode(this HtmlHelper html, string url, string alt = "QR code", int height = 200, int width = 200, int margin = 0)
+        {
+            var qrWriter = new BarcodeWriter();
+            qrWriter.Format = BarcodeFormat.QR_CODE;
+            qrWriter.Options = new EncodingOptions() { Height = height, Width = width, Margin = margin };
+
+            using (var q = qrWriter.Write(url))
+            {
+                using (var ms = new MemoryStream())
+                {
+                    q.Save(ms, ImageFormat.Png);
+                    var img = new TagBuilder("img");
+                    img.Attributes.Add("src", String.Format("data:image/png;base64,{0}", Convert.ToBase64String(ms.ToArray())));
+                    img.Attributes.Add("alt", alt);
+                    return MvcHtmlString.Create(img.ToString(TagRenderMode.SelfClosing));
+                }
+            }
+        }
+    }
+}
