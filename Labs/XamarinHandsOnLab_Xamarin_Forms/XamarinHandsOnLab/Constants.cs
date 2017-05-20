bfff553d6806b96f000052166a276a9fa1511331
李進興 Vulcan <vulcan@miniasp.com>
2017-05-20 14:34:13 +0800

    加入 Azure Mobile App 提供的後端與XF專案原始碼

---
+++ Labs/XamarinHandsOnLab_Xamarin_Forms/XamarinHandsOnLab/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace XamarinHandsOnLab
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://xamarinhandsonlab.azurewebsites.net";
+	}
+}
+
