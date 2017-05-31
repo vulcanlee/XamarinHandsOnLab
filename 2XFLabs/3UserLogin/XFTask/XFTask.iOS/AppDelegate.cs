fc596cbf075f27e0e55429c83531a98669935ab1
李進興 Vulcan <vulcan@miniasp.com>
2017-05-31 12:23:47 +0800

    安裝需要的 NuGet 套件

---
+++ 2XFLabs/3UserLogin/XFTask/XFTask.iOS/AppDelegate.cs
@@ -0,0 +1,53 @@
+﻿
+using Foundation;
+using UIKit;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+
+namespace XFTask.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            #region 第三方套件／插件的初始化
+            Plugin.Iconize.Iconize
+                .With(new Plugin.Iconize.Fonts.FontAwesomeModule());
+            FormsPlugin.Iconize.iOS.IconControls.Init();
+
+            // Initialize the Azure Mobile Client SDK
+            // http://stackoverflow.com/questions/24521355/azure-mobile-services-invalid-operation-exception-platform-specific-assembly-n
+            Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
+            #endregion
+
+            global::Xamarin.Forms.Forms.Init();
+
+            #region 要使用者允許接收通知之設定
+            #endregion
+
+            LoadApplication(new App(new iOSInitializer()));
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+
+    public class iOSInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IUnityContainer container)
+        {
+
+        }
+    }
+
+}
